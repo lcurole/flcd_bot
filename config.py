7086d89c23406dff8e5c602dbb7a06b743c876e3
Logan Curole <lcurole@gmail.com>
2022-03-15 16:48:00 -0400

    bot now uploads images to discord added config var for deal image header

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-DEALS_UPDATED_DISCORD_MESSAGE = f'{datetime.today().strftime("%m/%d/%Y")} New Deal!'
+DEALS_UPDATED_DISCORD_MESSAGE = f'New Deal! {datetime.today().strftime("%m/%d/%Y")}'
 DEALS_IMAGE_EMBED_HEADER_MESSAGE = 'New Deal!'
 DISCORD_WEBHOOK_BOT_NAME = 'FLCD Bot'
 DISCORD_WEBHOOK_AVATAR_URL = 'https://cdn.discordapp.com/avatars/869659791482568744/684ee1bfbb50ea3cd26a3a06c1a4a155.webp'
