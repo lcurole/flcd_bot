7e162679c6f30055267966b286d430013094eeb6
Logan Curole <lcurole@gmail.com>
2023-01-27 21:31:39 -0500

    add custom config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-DEALS_UPDATED_DISCORD_MESSAGE = f'New Deal! {datetime.today().strftime("%m/%d/%Y")}'
+DEALS_UPDATED_DISCORD_MESSAGE = f'<@&948814795786821643 Update'
 DEALS_IMAGE_EMBED_HEADER_MESSAGE = 'New Deal!'
 DISCORD_WEBHOOK_BOT_NAME = 'FLCD Bot'
-DISCORD_WEBHOOK_AVATAR_URL = 'https://cdn.discordapp.com/avatars/869659791482568744/684ee1bfbb50ea3cd26a3a06c1a4a155.webp'
+DISCORD_WEBHOOK_AVATAR_URL = 'https://i.imgur.com/dw0uEP8.png'
 TWITTER_STATUS_MESSAGE = ''
